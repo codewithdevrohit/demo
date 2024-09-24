b4f394502c96fac3a2c4d8f20e7efdfe6d52b3c4
codewithdevrohit <rsasane258@gmail.com>
2024-09-24 15:53:39 +0530

    first upload

---
+++ app.js
@@ -0,0 +1,34 @@
+console.log("this is from external js");
+
+//variable - its act like container 
+
+// var let const
+//let const 2016 es6
+
+var title = "KGF"
+let fullName = "shreyas s deshpande"
+const pi = 3.14
+
+console.log(title,fullName,pi);
+
+//datatype 
+// 1.prmitive / value type 
+// 2.non-prmitive / refarnce type
+
+//1.primitve /value type
+
+var name = "shreyas" //string
+
+var mobile = 8055812454 //number
+
+var isDay = false //boolean
+
+//2.non-prmitive / refernce type
+
+//array
+var movies = ["lord of the rings","lotr 2","kgf"]
+console.log(movies[2]);
+
+//object
+var student = {name:"ram",class:"msc"}
+console.log(student.name,student.class);
